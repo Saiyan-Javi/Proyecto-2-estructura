4ea9cccfe4966e2a071f08903a3bb3e538dbcfa1
Saiyan-Javi <ova22103@uvg.edu.gt>
2023-05-07 21:40:55 -0600

    opcion para pedir salsa

--- PizzaMatcher.java
+++ PizzaMatcher.java
@@ -42,13 +42,45 @@ public class PizzaMatcher {
 
     System.out.println("¿Que ingredientes desearía que lleve la salsa?");
 
-
+    Map<String, ArrayList<String>> salsas = new HashMap<>();
+    // Agregar salsas y sus ingredientes
+    salsas.put("base orégano", new ArrayList<>(Arrays.asList("oregano", "aceite", "tomate")));
+    salsas.put("picante", new ArrayList<>(Arrays.asList("jalapeño", "tomate")));
+    salsas.put("hongos", new ArrayList<>(Arrays.asList("tomate", "pimiento", "champiñones")));
+    salsas.put("blanca", new ArrayList<>(Arrays.asList("crema", "queso", "tomillo", "verduras")));
+    salsas.put("pesto", new ArrayList<>(Arrays.asList("cilantro", "albahaca", "ajo", "verduras")));
+    salsas.put("ajo", new ArrayList<>(Arrays.asList("aciete de oliva", "ajo")));
+    // Pedir los ingredientes de la pizza
+    String inputString1 = input.nextLine();
+    String[] inputArray1 = inputString1.split(",");
+    ArrayList<String> inputList1 = new ArrayList<>(Arrays.asList(inputArray1));
+    // Comparar los ingredientes de la pizza ingresados con los ingredientes de las salsas disponibles
+    int maxCount1 = -1;
+    String maxSalsa = "";
+    for (Map.Entry<String, ArrayList<String>> entry : salsas.entrySet()) {
+      ArrayList<String> salsaIngredients = entry.getValue();
+      int count1 = 0;
+      for (String ingredient1 : inputList1) {
+        if (salsaIngredients.contains(ingredient1.trim())) {
+          count1++;
+        }
+      }
+      if (count1 > maxCount1) {
+        maxCount1 = count1;
+        maxSalsa = entry.getKey();
+      }
+    }
 
     // Mostrar la pizza que mejor coincide
-    if (maxCount == 0) {
+    if (maxCount == 0 ) {
       System.out.println("Lo sentimos, no tenemos una pizza con los ingredientes que ha proporcionado.");
     } else {
-      System.out.println("La pizza que mejor coincide es: " + maxPizza + " de maza " + op);
+        if (maxCount1 == 0){
+        System.out.println("Lo sentimos, no tenemos una salsa con los ingredientes que ha proporcionado.");
+        System.out.println("La pizza que mejor coincide es: " + maxPizza + " de maza " + op);
+      } else{
+        System.out.println("La pizza que mejor coincide es: " + maxPizza + " de maza " + op + " con una base de salsa de " + maxSalsa);
+      } 
     }
   }
   

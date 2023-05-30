975a6ddc1b2d232f94ece3965ba98b373142d3b7
Juan Luis <87204153+JusSolo@users.noreply.github.com>
2023-05-30 12:45:51 -0600

    limpieza
    lipieza de código

--- PizzaMatcher.java
+++ PizzaMatcher.java
@@ -21,78 +21,6 @@ public class PizzaMatcher {
     EmbeddedNeo4j database = new EmbeddedNeo4j("bolt://localhost:7687", "neo4j", "hola12345");
     Scanner input = new Scanner(System.in);
 
-    /*
-
-
-
-
-
-
-
-
-
-
-    System.out.println("\n**************************************************\n" +
-            "* ¿Que ingredientes desearía que lleve la salsa? *\n" +
-            "**************************************************");
-
-    Map<String, ArrayList<String>> salsas = new HashMap<>();
-    // Agregar salsas y sus ingredientes
-    salsas.put("base orégano", new ArrayList<>(Arrays.asList("oregano", "aceite", "tomate")));
-    salsas.put("picante", new ArrayList<>(Arrays.asList("jalapeño", "tomate")));
-    salsas.put("hongos", new ArrayList<>(Arrays.asList("tomate", "pimiento", "champiñones")));
-    salsas.put("blanca", new ArrayList<>(Arrays.asList("crema", "queso", "tomillo", "verduras")));
-    salsas.put("pesto", new ArrayList<>(Arrays.asList("cilantro", "albahaca", "ajo", "verduras")));
-    salsas.put("ajo", new ArrayList<>(Arrays.asList("aciete de oliva", "ajo")));
-    // Pedir los ingredientes de la pizza
-    String inputString1 = input.nextLine();
-    String[] inputArray1 = inputString1.split(",");
-    ArrayList<String> inputList1 = new ArrayList<>(Arrays.asList(inputArray1));
-    // Comparar los ingredientes de la pizza ingresados con los ingredientes de las salsas disponibles
-    int maxCount1 = -1;
-    String maxSalsa = "";
-    for (Map.Entry<String, ArrayList<String>> entry : salsas.entrySet()) {
-      ArrayList<String> salsaIngredients = entry.getValue();
-      int count1 = 0;
-      for (String ingredient1 : inputList1) {
-        if (salsaIngredients.contains(ingredient1.trim())) {
-          count1++;
-        }
-      }
-      if (count1 > maxCount1) {
-        maxCount1 = count1;
-        maxSalsa = entry.getKey();
-      }
-    }
-
-    // Mostrar la pizza que mejor coincide
-    if (maxCount == 0 ) {
-      System.out.println("\n********************************************************************************\n" +
-              "* Lo sentimos, no tenemos una pizza con los ingredientes que ha proporcionado. *\n" +
-              "********************************************************************************");
-    } else {
-        if (maxCount1 == 0){
-        System.out.println("********************************************************************************\n" +
-                "* Lo sentimos, no tenemos una salsa con los ingredientes que ha proporcionado. *\n" +
-                "********************************************************************************");
-        System.out.println("*\n * La pizza que mejor coincide es: " + maxPizza + " de maza " + op);
-      } else{
-        System.out.println("*\n * La pizza que mejor coincide es: " + maxPizza + " de maza " + op + " con una base de salsa de " + maxSalsa);
-      } 
-    }
-
-*/
-
-    // Agregar pizzas y sus ingredientes
-    /* 
-    
-    pizzas.put("Pepperoni", new ArrayList<>(Arrays.asList("pepperoni", "queso", "tomate")));
-    pizzas.put("Hawaiana", new ArrayList<>(Arrays.asList("piña", "jamon", "queso")));
-    pizzas.put("Vegetariana", new ArrayList<>(Arrays.asList("cebolla", "pimiento verde", "champiñones", "verduras")));
-    pizzas.put("5 carnes", new ArrayList<>(Arrays.asList("jamon", "salchicha", "pepperoni", "salami", "carne molida")));
-    pizzas.put("Super Suprema", new ArrayList<>(Arrays.asList("cebolla", "aceitunas", "champiñones", "chile verde", "carne de cerdo")));
-    */
-
     int op = 0;
     do {
       System.out.println("***************************************************************\n" +
@@ -115,7 +43,7 @@ public class PizzaMatcher {
 
         System.out.print("\n* Ingrese la pizza que le gusta:");
         String p = input.nextLine();
-        //crear metodo en EmbeddedNeo4j.java para recomendar pizza basado en los ingredientes de la pizza ingresada
+
           System.out.println("Pizzas que le podrían gustar incluyen:");
 
           LinkedList<String> pizzaList2 = database.getSimilarPizzas(p);
@@ -154,36 +82,15 @@ public class PizzaMatcher {
           System.out.println(b);
         }
         break;
-        case 4: //salir
+        case 3: //salir
           System.out.println("Gracias por su tiempo");
           break;
 
         default:
           System.out.print("Esta opción no es válida por favor reintentar \n");
           break;
-
-        // Comparar los ingredientes de la pizza ingresados con los ingredientes de las pizzas disponibles
-       /* int maxCount = -1;
-        String maxPizza = "";
-        for (Map.Entry<String, ArrayList<String>> entry : pizzas.entrySet()) {
-          ArrayList<String> pizzaIngredients = entry.getValue();
-          int count = 0;
-          for (String ingredient : inputList) {
-            if (pizzaIngredients.contains(ingredient.trim())) {
-              count++;
-            }
-          }
-          if (count > maxCount) {
-            maxCount = count;
-            maxPizza = entry.getKey();
-          }
-        }
-        */
       }
     }while( op != 3);
-
-    //System.out.print("\n Ingrese cuanto paga por su pizza usualmente: ");
-
 
     input.close();
 
